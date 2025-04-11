c6544cf0dc8e96f02b34a5ba6e830a55e4c5f933
KachanVlg <vlad.bend@inbox.ru>
2025-04-11 14:13:13 +0300

    Initial commit

---
+++ src/test/java/com/example/wildman_backend/WildmanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wildman_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildmanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
