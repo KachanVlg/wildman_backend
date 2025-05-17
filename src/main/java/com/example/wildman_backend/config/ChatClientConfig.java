5527595b1c92397bbbf4f10612aac977a15b90de
KachanVlg <vlad.bend@inbox.ru>
2025-05-17 18:27:21 +0300

    Создан контроллер чата

--- src/main/java/com/example/wildman_backend/config/ChatClientConfig.java
+++
@@ -1,4 +0,0 @@
-package com.example.wildman_backend.config;
-
-public class ChatClientConfig {
-}
--- src/main/java/com/example/wildman_backend/controller/ChatController.java
+++ src/main/java/com/example/wildman_backend/controller/ChatController.java
@@ -2,46 +2,29 @@ package com.example.wildman_backend.controller;
 
 import com.example.wildman_backend.domain.dto.ChatDto;
 import com.example.wildman_backend.domain.dto.MessageDto;
+import com.example.wildman_backend.service.ChatService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.MessageType;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/chats")
 @RequiredArgsConstructor
 public class ChatController {
-
-    private final OpenAiChatModel chatClient;
-    private final ChatMemory chatMemory;
-
+    private final ChatService chatService;
 
     @PostMapping
-    public ChatDto createChat(){
-        return new ChatDto(UUID.randomUUID().toString());
+    public ChatDto startChat(){
+        return chatService.startChat();
     }
 
     @PostMapping("/{id}/message")
     public MessageDto processMessage(@PathVariable("id") String chatId,
                                      @RequestBody MessageDto dto){
-        dto.setText("Сгенерируй ответ на данное сообщение, как будто ты ведешь живой диалог, сгенерируй на английском " + dto.getText());
-        chatMemory.add(chatId, dto);
-        Prompt prompt = new Prompt(chatMemory.get(chatId));
-        String response = chatClient.call(prompt).getResult().getOutput().getText();
-        MessageDto chatResponse = MessageDto.builder()
-                .text(response)
-                .type(MessageType.ASSISTANT)
-                .build();
-        chatMemory.add(chatId, dto);
-        return chatResponse;
+        return chatService.sendMessage(chatId, dto);
     }
 
-
-
+    @PostMapping("{id}/end_chat")
+    public void endChat(@PathVariable("id") String chatId) {
+        chatService.endChat(chatId);
+    }
 }
