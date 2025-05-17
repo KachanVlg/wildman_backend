59d9d0c64600d873fb62568de58502ef0a7b0ba7
KachanVlg <vlad.bend@inbox.ru>
2025-05-17 12:12:29 +0300

    Добавлен DataSeeder (можно закомментить при необходимости)

---
+++ src/main/java/com/example/wildman_backend/repository/DataSeeder.java
@@ -0,0 +1,24 @@
+package com.example.wildman_backend.repository;
+
+import com.example.wildman_backend.domain.model.user.Role;
+import com.example.wildman_backend.domain.model.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class DataSeeder implements CommandLineRunner {
+    private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+        userRepository.save(User.builder()
+                .role(Role.USER)
+                .username("user")
+                .password(encoder.encode("user")).build());
+    }
+}
