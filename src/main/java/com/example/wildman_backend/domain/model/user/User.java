29db76b3ae2103d74d3f26eb0d9e2f0e42ce056d
KachanVlg <vlad.bend@inbox.ru>
2025-10-11 21:49:51 +0300

    User entity added

---
+++ src/main/java/com/example/wildman_backend/domain/model/user/User.java
@@ -0,0 +1,33 @@
+package com.example.wildman_backend.domain.model.user;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "password")
+    private String password;
+
+
+
+}
